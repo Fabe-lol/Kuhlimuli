601392384fa9f230fc155e3d723c8d5df96dc3cd
Fabe <fabiab.biechl@t-online.de>
2020-05-22 09:39:03 +0200

    erster Save test im Dashboard fragment

--- KuhleKuh/app/src/main/java/com/example/kuhlekuh/ui/dashboard/DashboardFragment.java
+++ KuhleKuh/app/src/main/java/com/example/kuhlekuh/ui/dashboard/DashboardFragment.java
@@ -1,35 +1,105 @@
 package com.example.kuhlekuh.ui.dashboard;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
-
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
-
 import com.example.kuhlekuh.R;
+import com.example.kuhlekuh.ui.home.HomeViewModel;
+
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Switch;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import static android.content.Context.MODE_PRIVATE;
 
 public class DashboardFragment extends Fragment {
 
     private DashboardViewModel dashboardViewModel;
+    private TextView textView;
+    private EditText editText;
+    private Button applyTextButton;
+    private Button saveButton;
+    private Switch switch1;
 
+    public static final String SHARED_PREFS = "sharedPrefs";
+    public static final String TEXT = "text";
+    public static final String SWITCH1 = "switch1";
+
+    private String text;
+    private boolean switchOnOff;
+    private HomeViewModel homeViewModel;
+
+
+    @Override
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        dashboardViewModel =
-                ViewModelProviders.of(this).get(DashboardViewModel.class);
+        homeViewModel =
+                ViewModelProviders.of(this).get(HomeViewModel.class);
         View root = inflater.inflate(R.layout.fragment_dashboard, container, false);
-        final TextView textView = root.findViewById(R.id.text_dashboard);
-        dashboardViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+
+        textView = (TextView) root.findViewById(R.id.textview);
+        editText = (EditText) root.findViewById(R.id.edittext);
+        applyTextButton = (Button) root.findViewById(R.id.apply_text_button);
+        saveButton = (Button) root.findViewById(R.id.save_button);
+        switch1 = (Switch) root.findViewById(R.id.switch1);
+
+        applyTextButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
+            public void onClick(View view) {
+                textView.setText(editText.getText().toString());
             }
         });
+
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                saveData();
+            }
+        });
+
+        loadData();
+        updateViews();
         return root;
+    }
+
+    private void setContentView(int fragment_dashboard) {
+    }
+
+    public void saveData() {
+        SharedPreferences sharedpreferences = getActivity().getSharedPreferences(SHARED_PREFS, Context.MODE_PRIVATE);
+
+        SharedPreferences.Editor editor = sharedpreferences.edit();
+
+        editor.putString(TEXT, textView.getText().toString());
+        editor.putBoolean(SWITCH1, switch1.isChecked());
+
+        editor.apply();
+
+        Toast.makeText(getActivity(), "Data saved", Toast.LENGTH_SHORT).show();
+    }
+
+    public void loadData() {
+        SharedPreferences sharedpreferences = getActivity().getSharedPreferences(SHARED_PREFS, MODE_PRIVATE);
+        text = sharedpreferences.getString(TEXT, "");
+        switchOnOff = sharedpreferences.getBoolean(SWITCH1, false);
+    }
+
+    public void updateViews() {
+        textView.setText(text);
+        switch1.setChecked(switchOnOff);
+
     }
 }
