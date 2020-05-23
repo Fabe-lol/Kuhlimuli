e6dda5e85025b739665cc06527cb29c736e9a3f4
Heinrich <heinrich.ostermeier@gmx.de>
2020-05-23 22:06:07 +0200

    arbitrary amount of rows in the table

--- KuhleKuh/app/src/main/java/com/example/kuhlekuh/ui/notifications/NotificationsFragment.java
+++ KuhleKuh/app/src/main/java/com/example/kuhlekuh/ui/notifications/NotificationsFragment.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TableLayout;
+import android.widget.TableRow;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -23,13 +25,26 @@ public class NotificationsFragment extends Fragment {
         notificationsViewModel =
                 ViewModelProviders.of(this).get(NotificationsViewModel.class);
         View root = inflater.inflate(R.layout.fragment_notifications, container, false);
-        final TextView textView = root.findViewById(R.id.text_notifications);
-        notificationsViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
+
+        TableLayout prices = (TableLayout)root.findViewById(R.id.simpleTableLayout);
+        prices.setStretchAllColumns(true);
+        prices.bringToFront();
+        for(int i = 0; i < 3; i++){
+            TableRow tr =  new TableRow(getContext());
+            TextView c1 = new TextView(getContext());
+            c1.setText("cowNumber");
+            TextView c2 = new TextView(getContext());
+            c2.setText("vaccination");
+            TextView c3 = new TextView(getContext());
+            c3.setText("muh");
+            tr.addView(c1);
+            tr.addView(c2);
+            tr.addView(c3);
+            prices.addView(tr);
+        }
+
         return root;
     }
 }
+
+
