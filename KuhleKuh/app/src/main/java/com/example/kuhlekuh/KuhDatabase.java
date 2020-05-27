8a9648e65ba11ddd9e6731ccc2c5d201b102813c
Heinrich <heinrich.ostermeier@gmx.de>
2020-05-27 19:40:13 +0200

    create database. Insert and update.

--- KuhleKuh/app/src/main/java/com/example/kuhlekuh/KuhDatabase.java
+++ KuhleKuh/app/src/main/java/com/example/kuhlekuh/KuhDatabase.java
@@ -4,6 +4,6 @@ import androidx.room.Database;
 import androidx.room.RoomDatabase;
 
 @Database(entities = {Kuh.class}, version = 1)
-public abstract class AppDatabase extends RoomDatabase {
+public abstract class KuhDatabase extends RoomDatabase {
     public abstract KuhDao userDao();
 }
